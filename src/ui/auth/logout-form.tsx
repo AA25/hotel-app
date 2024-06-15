9d747ec1f9954a002e16cde7671d8418ff03672d
Ade <ade.akingbade@citypantry.com>
2024-06-15 12:50:05 +0100

    Added form to add new hotels

--- src/ui/auth/logout-form.tsx
+++ src/ui/auth/logout-form.tsx
@@ -3,9 +3,9 @@ import { handleSignOut } from "@/lib/cognitoActions";
 
 export default function LogoutForm() {
     return (
-        <form action={handleSignOut}>
-            <button>
-                <div>Sign Out</div>
+        <form action={handleSignOut} className="mb-3">
+            <button type="submit" className="btn btn-warning">
+                Sign Out
             </button>
         </form>
     );
